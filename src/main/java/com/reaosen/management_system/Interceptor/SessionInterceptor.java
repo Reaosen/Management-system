6233108eef8f4ac89b1e19bb95881d888ba1c403
Reaosen <qls18230040072@163.com>
2025-01-23 17:22:04 +0800

    添加拦截器相关，以及未完成的未读通知功能

---
+++ src/main/java/com/reaosen/management_system/Interceptor/SessionInterceptor.java
@@ -0,0 +1,52 @@
+package com.reaosen.management_system.Interceptor;
+
+import com.reaosen.management_system.Mapper.UserMapper;
+import com.reaosen.management_system.Model.User;
+import com.reaosen.management_system.Model.UserExample;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Service
+public class SessionInterceptor implements HandlerInterceptor {
+    @Autowired
+    private UserMapper userMapper;
+    // TODO 未读通知相关
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Cookie[] cookies = request.getCookies();
+        if ((cookies != null && cookies.length != 0)) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    UserExample userExample = new UserExample();
+                    userExample.createCriteria()
+                            .andTokenEqualTo(token);
+                    List<User> users = userMapper.selectByExample(userExample);
+                    if (users.size() != 0) {
+                        request.getSession().setAttribute("user", users.get(0));
+                    }
+                    break;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
